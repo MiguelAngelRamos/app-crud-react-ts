48768a097cbf2ffbf65d3d997201b37659c1e197
Miguel Ramos <return.infox@gmail.com>
2023-11-03 18:53:54 -0300

    instalando bootstrap

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,7 +1,23 @@
 const Login: React.FC = () => {
+
   return (
-    <div>Login</div>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col">
+          <form>
+
+            <div className="mb-3">
+              <label htmlFor="" className="">Username</label>
+            </div>
+
+            <div className="mb-3" >
+              <label htmlFor="" className=""></label>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Login
+export default Login;
