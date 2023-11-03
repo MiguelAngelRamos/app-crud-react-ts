6f3ce2347c3e5aab612c5e8752c1fc19a4705e6b
Miguel Ramos <return.infox@gmail.com>
2023-11-03 19:29:51 -0300

    registro formulario con formik

--- src/views/register/Register.tsx
+++ src/views/register/Register.tsx
@@ -1,16 +1,34 @@
+import { useFormik } from 'formik';
 
-const Register = () => {
+const Register: React.FC = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      confirmPassword: '',
+      role: '',
+     
+    }
+  });
   return (
     <div className="container mt-5">
     <div className="row">
       <h1 className="text-center">Register</h1>
+
       <div className="col-6 offset-3">
-        <form>
+
+        <form onSubmit={formik.handleSubmit}>
+
           <div className="mb-3">
             <label htmlFor="" className="form-label">Username</label>
             <input 
               type="text"
               className="form-control"
+              id="username"
+              name="username"
+              onChange={formik.handleChange}
+              value={formik.values.username}
                />
           </div>
 
@@ -19,22 +37,34 @@ const Register = () => {
             <input 
               type="text"
               className="form-control"
+              id="role"
+              name="role"
+              onChange={formik.handleChange}
+              value={formik.values.role}
                />
           </div>
 
           <div className="mb-3" >
-            <label htmlFor="" className="form-label">Contraseña</label>
+            <label htmlFor="" className="form-label">Password</label>
             <input 
               type="password"
               className="form-control"
+              id="password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
             />
           </div>
 
           <div className="mb-3" >
-            <label htmlFor="" className="form-label">Confirmar Contraseña</label>
+            <label htmlFor="" className="form-label">Repeat Password</label>
             <input 
               type="password"
               className="form-control"
+              id="confirmPassword"
+              name="confirmPassword"
+              onChange={formik.handleChange}
+              value={formik.values.confirmPassword}
             />
           </div>
 
@@ -43,6 +73,8 @@ const Register = () => {
             <input 
               type="file"
               className="form-control"
+              name="image"
+              
             />
           </div>
 
