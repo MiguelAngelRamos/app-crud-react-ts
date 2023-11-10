7b02fd6d09b60eef0c5c5e266c244e05d1739d94
Miguel Angel Ramos <return.infox@gmail.com>
2023-11-10 19:14:23 -0300

    renderizacion de los estudiantes

--- src/views/students/Students.tsx
+++ src/views/students/Students.tsx
@@ -1,7 +1,59 @@
-const Students = () => {
+import { useState, useEffect } from "react";
+import { IStudent } from "../../interfaces/IStudent";
+import axios from "axios";
+
+const Students: React.FC = () => {
+
+  const [students, setStudents] = useState<IStudent[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+
+    const token = localStorage.getItem('authToken');
+
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    axios.get(`http://localhost:3000/students`, { headers })
+      .then(response => {
+        setStudents(response.data);
+      }).catch(err => {
+        if (axios.isAxiosError(err)) {
+          setError(err.response?.data?.error || 'Error inesperado')
+        }
+      })
+  }, []);
+
+  if (error) return <p>{error}</p>
   return (
-    <div>Students - has accedido al sistema!!</div>
+
+    <div className="container mt-5">
+      <h2 className="mb-2">Listado de Estudiantes</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody className="table-group-divider">
+          {students.map(student => (
+            <tr>
+              <td>{student.name}</td>
+              <td>{student.age}</td>
+              <td>Eliminar - Update</td>
+           </tr>
+          ))}
+     
+        </tbody>
+      </table>
+    </div>
+
   )
+
+
 }
 
 export default Students;
