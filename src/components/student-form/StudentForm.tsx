add36aaa455f17edcccc58e5d15b152ebccd9d35
Miguel Angel Ramos <return.infox@gmail.com>
2023-11-14 19:22:50 -0300

    crud completo

--- src/components/student-form/StudentForm.tsx
+++ src/components/student-form/StudentForm.tsx
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
-import { IStudent } from "../../interfaces/IStudent";
+import { useNavigate, useParams } from "react-router-dom"
+// import { IStudent } from "../../interfaces/IStudent";
 
 const StudentForm = () => {
-  const [student, setStudent] = useState<IStudent>();
+  const [student, setStudent] = useState({name: '', age: 0});
   const { id } = useParams();
+  const navigate = useNavigate();
 
-  const isEditMode = id !== undefined;
-
+  const isEditMode:boolean = id !== undefined;
+ 
+  console.log(student);
   useEffect(() => {
     if(isEditMode) {
       const getStudentData = async () => {
@@ -27,31 +29,54 @@ const StudentForm = () => {
     }
   }, [id, isEditMode])
 
+  const handleSave = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem('authToken');
+      const headers = { Authorization: `Bearer ${token}`};
 
+      if(isEditMode) {
+        await axios.put(`http://localhost:3000/students/${id}`, student, {headers})
+      } else {
+        await axios.post('http://localhost:3000/students', student, {headers})
+      }
+      navigate('/students')
+    } catch (error) {
+      console.error("Error al guardar al estudiante" + error);
+    }
+   
+
+  }
   //* otro modo mas 
   return (
     <div className="container mt-5">
-      <h2>Editar Estudiante</h2>
-      <form >
+      <h2>Formulario de Student</h2>
+      <form onSubmit={handleSave}>
         <div className="mb-3">
           <label htmlFor="">Nombre:</label>
           <input 
             type="text" 
             className="form-control"
+            id="studentName"
             value={student?.name}
+            onChange={(e) => setStudent({...student, name: e.target.value})}
             />
         </div>
 
         <div className="mb-3">
-          <label htmlFor="">Nombre:</label>
+          <label htmlFor="">Edad:</label>
           <input 
             type="number" 
             className="form-control"
+            id="studentAge"
             value={student?.age}
+            onChange={(e) => setStudent({...student, age: parseInt(e.target.value, 10) || 0 })}
             />
         </div>
         <button type="submit" className="btn btn-primary">Guardar</button>
       </form>
+
+      
     </div>
   )
 }
